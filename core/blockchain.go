c07a4007d76f2a9d9841d338e583a21b93df2b8b
muyinliu <muyinliu@gmail.com>
2021-09-29 18:14:51 +0800

    Set ContentType to application/json of message publish to RabbitMQ

--- core/blockchain.go
+++ core/blockchain.go
@@ -987,7 +987,7 @@ func (bc *BlockChain) sendBlockToDownstream(block *types.Block) {
 			false,
 			amqp.Publishing{
 				Headers:         amqp.Table{},
-				ContentType:     "text/plain",
+				ContentType:     "application/json",
 				ContentEncoding: content_encoding,
 				Body:            message_data,
 				DeliveryMode:    amqp.Persistent,
